760053063fae6733dff7749db66d09906f761d7a
kyutik <yutasuzuya@gmail.com>
2026-05-28 00:05:09 +0300

    Update Program.cs

--- Lab15/Program.cs
+++ Lab15/Program.cs
@@ -8,9 +8,6 @@ namespace Lab15
 {
     internal static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
         [STAThread]
         static void Main()
         {
