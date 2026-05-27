57c239fc8af3103d39ee49b6b3021649c6a04a31
kyutik <yutasuzuya@gmail.com>
2026-05-28 00:08:18 +0300

    Update Form1.Designer.cs

--- Lab15/Form1.Designer.cs
+++ Lab15/Form1.Designer.cs
@@ -2,15 +2,9 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// Обязательная переменная конструктора.
-        /// </summary>
+
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Освободить все используемые ресурсы.
-        /// </summary>
-        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,12 +14,7 @@
             base.Dispose(disposing);
         }
 
-        #region Код, автоматически созданный конструктором форм Windows
 
-        /// <summary>
-        /// Требуемый метод для поддержки конструктора — не изменяйте 
-        /// содержимое этого метода с помощью редактора кода.
-        /// </summary>
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
@@ -49,9 +38,7 @@
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // tabControl1
-            // 
+
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Controls.Add(this.tabPage3);
@@ -66,9 +53,7 @@
             this.tabControl1.SelectedIndex = 0;
             this.tabControl1.Size = new System.Drawing.Size(1020, 373);
             this.tabControl1.TabIndex = 0;
-            // 
-            // tabPage1
-            // 
+
             this.tabPage1.Controls.Add(this.button1);
             this.tabPage1.Controls.Add(this.textBox3);
             this.tabPage1.Controls.Add(this.textBox2);
@@ -86,9 +71,7 @@
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Завдання 1";
             this.tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // tabPage2
-            // 
+
             this.tabPage2.Location = new System.Drawing.Point(4, 27);
             this.tabPage2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabPage2.Name = "tabPage2";
@@ -97,9 +80,7 @@
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Завдання 2";
             this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // tabPage3
-            // 
+
             this.tabPage3.Location = new System.Drawing.Point(4, 27);
             this.tabPage3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabPage3.Name = "tabPage3";
@@ -108,9 +89,7 @@
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Завдання 3";
             this.tabPage3.UseVisualStyleBackColor = true;
-            // 
-            // tabPage4
-            // 
+
             this.tabPage4.Location = new System.Drawing.Point(4, 27);
             this.tabPage4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabPage4.Name = "tabPage4";
@@ -119,9 +98,7 @@
             this.tabPage4.TabIndex = 3;
             this.tabPage4.Text = "Завдання 4";
             this.tabPage4.UseVisualStyleBackColor = true;
-            // 
-            // tabPage5
-            // 
+
             this.tabPage5.Location = new System.Drawing.Point(4, 27);
             this.tabPage5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabPage5.Name = "tabPage5";
@@ -130,9 +107,7 @@
             this.tabPage5.TabIndex = 4;
             this.tabPage5.Text = "Завдання 5";
             this.tabPage5.UseVisualStyleBackColor = true;
-            // 
-            // tabPage6
-            // 
+
             this.tabPage6.Location = new System.Drawing.Point(4, 27);
             this.tabPage6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabPage6.Name = "tabPage6";
@@ -141,9 +116,7 @@
             this.tabPage6.TabIndex = 5;
             this.tabPage6.Text = "Завдання 6";
             this.tabPage6.UseVisualStyleBackColor = true;
-            // 
-            // tabPage7
-            // 
+
             this.tabPage7.Location = new System.Drawing.Point(4, 27);
             this.tabPage7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabPage7.Name = "tabPage7";
@@ -152,9 +125,7 @@
             this.tabPage7.TabIndex = 6;
             this.tabPage7.Text = "Завдання 7";
             this.tabPage7.UseVisualStyleBackColor = true;
-            // 
-            // pictureBox1
-            // 
+
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(4, 0);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
@@ -162,9 +133,7 @@
             this.pictureBox1.Size = new System.Drawing.Size(1000, 113);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
-            // 
-            // label1
-            // 
+
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(24, 129);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
@@ -172,9 +141,7 @@
             this.label1.Size = new System.Drawing.Size(163, 18);
             this.label1.TabIndex = 1;
             this.label1.Text = "Введіть значення х:";
-            // 
-            // label2
-            // 
+ 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(24, 174);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
@@ -182,9 +149,7 @@
             this.label2.Size = new System.Drawing.Size(163, 18);
             this.label2.TabIndex = 2;
             this.label2.Text = "Введіть значення у:";
-            // 
-            // label3
-            // 
+
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(24, 240);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
@@ -192,27 +157,21 @@
             this.label3.Size = new System.Drawing.Size(187, 18);
             this.label3.TabIndex = 3;
             this.label3.Text = "Результат розрахунку:";
-            // 
-            // textBox1
-            // 
+
             this.textBox1.Location = new System.Drawing.Point(291, 121);
             this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(128, 24);
             this.textBox1.TabIndex = 4;
             this.textBox1.Text = "0,0";
-            // 
-            // textBox2
-            // 
+
             this.textBox2.Location = new System.Drawing.Point(291, 168);
             this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(130, 24);
             this.textBox2.TabIndex = 5;
             this.textBox2.Text = "0,0";
-            // 
-            // textBox3
-            // 
+
             this.textBox3.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.textBox3.Enabled = false;
             this.textBox3.Location = new System.Drawing.Point(216, 237);
@@ -220,9 +179,7 @@
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(213, 24);
             this.textBox3.TabIndex = 6;
-            // 
-            // button1
-            // 
+
             this.button1.BackColor = System.Drawing.Color.Gainsboro;
             this.button1.Cursor = System.Windows.Forms.Cursors.Default;
             this.button1.Location = new System.Drawing.Point(644, 267);
@@ -233,9 +190,7 @@
             this.button1.Text = "Розрахувати";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // Form1
-            // 
+
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1032, 383);
